525c297e69d1f79c7563c136ccecacdb89ca6ec5
raghav.ramesh <raghav.ramesh@66f4bc34-1353-0410-bdfa-e5f918ec5882>
2008-12-07 17:56:39 +0000

    Adding javadoc

--- TestRR/src/java/com/ragstorooks/testrr/ScenarioListener.java
+++ TestRR/src/java/com/ragstorooks/testrr/ScenarioListener.java
@@ -19,7 +19,21 @@ along with TestRR.  If not, see <http://www.gnu.org/licenses/>.
     	
 package com.ragstorooks.testrr;
 
+/**
+ * An interface used to record the success or failure of any given scenario. This is implemented by the Runner class, and as such,
+ * need not be used by any scenario, except to notify the status of a scenario.
+ */
 public interface ScenarioListener {
+    /**
+     * Report a scenario as successful to the listener.
+     * @param scenarioId the id of the successful scenario.
+     */
 	void scenarioSuccess(String scenarioId);
+	
+    /**
+     * Report a scenario as failed to the listener.
+     * @param scenarioId the id of the failed scenario.
+     * @param message a message describing the failed scenario. This could either be the reason for failure, or the last known successful state. 
+     */
 	void scenarioFailure(String scenarioId, String message);
 }
