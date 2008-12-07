c41360f7abe6da59cd762083dfe6d8226776ea7d
raghav.ramesh <raghav.ramesh@66f4bc34-1353-0410-bdfa-e5f918ec5882>
2008-12-07 17:56:39 +0000

    Adding javadoc

--- trunk/TestRR/src/java/com/ragstorooks/testrr/Runner.java
+++ trunk/TestRR/src/java/com/ragstorooks/testrr/Runner.java
@@ -30,15 +30,20 @@ import java.util.concurrent.TimeUnit;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+/**
+ * Runner is the main class which does all the work for TestRR. Scenarios are executed concurrently to simulate a load
+ * on the system under test. The number of times a scenario is executed depends on its weighting. The preferred mode of
+ * execution can be configured explicitly using the exposed parameters.  
+ */
 public class Runner implements ScenarioListener {
 	private static final Log log = LogFactory.getLog(Runner.class);
 	private static final int HUNDRED = 100;
 	private static final Random randomGenerator = new Random();
 
-	private int numberOfRuns;
-    private int numberOfConcurrentStarts;
-    private long coolDownPeriod;
-    private boolean synchronizedScheduling;
+	private int numberOfRuns = 500;
+    private int numberOfConcurrentStarts = 5;
+    private long coolDownPeriod = 1000;
+    private boolean synchronizedScheduling = false;
     private int synchronizeWaitMilliSeconds = 1000;
     private long totalRunTimeMilliSeconds = 0;
     
@@ -53,6 +58,9 @@ public class Runner implements ScenarioListener {
     private CountDownLatch scenarioCompleteLatch;
     
     
+    /**
+     * To be invoked when all the parameters have been configured, to generate the load on the system under test.
+     */
     public void run() {
     	log.info(String.format("RPRunner(numberOfRuns=%d, numberOfConcurrentStarts=%d)", numberOfRuns, numberOfConcurrentStarts));
     	
@@ -71,18 +79,43 @@ public class Runner implements ScenarioListener {
 
     }
 
+    /**
+     * Get the overall rate of success for the executed scenarios.
+     * @return the success percentage as a value between 0 and 100.
+     */
     public double getSuccessRate() {
     	return 100.0*scenarioSuccesses.size()/numberOfRuns;
     }
     
+    /**
+     * Get information about the successful scenarios.
+     * @return a map keyed on scenario id, containing information about the successful scenarios.
+     */
     public Map<String, ScenarioResult> getScenarioSuccesses() {
     	return scenarioSuccesses;
     }
     
+    /**
+     * Get information about the failed scenarios
+     * @return a map keyed on scenario id, containing information about the failed scenarios.
+     */
     public Map<String, ScenarioResult> getScenarioFailures() {
     	return scenarioFailures;
     }
     
+    /**
+     * Get the total run time 
+     * @return the total time, in milliseconds, taken to run all scenarios, including the cool down period
+     */
+    public long getTotalRunTimeMilliSeconds() {
+        return totalRunTimeMilliSeconds;
+    }
+    
+    /**
+     * Report a scenario as failed to the runner.
+     * @param scenarioId the id of the failed scenario.
+     * @param message a message describing the failed scenario. This could either be the reason for failure, or the last known successful state. 
+     */
     public void scenarioFailure(String scenarioId, String message) {
     	ScenarioResult result = scenarioFailures.get(scenarioId);
     	log.info(String.format("Scenario %s (%s) failed", scenarioId, result.getScenarioType().getSimpleName()));
@@ -91,6 +124,10 @@ public class Runner implements ScenarioListener {
     	notifyCountDownLatch();
     }
 
+    /**
+     * Report a scenario as successful to the runner.
+     * @param scenarioId the id of the successful scenario.
+     */
     public void scenarioSuccess(String scenarioId) {
     	ScenarioResult result = scenarioFailures.remove(scenarioId);
     	log.info(String.format("Scenario %s (%s) succeeded", scenarioId, result.getScenarioType().getSimpleName()));
@@ -189,38 +226,72 @@ public class Runner implements ScenarioListener {
 		}
 	}
 	
+	/**
+	 * Set numberBeforeRuns before execution.
+	 * @param numberOfRuns the total number of scenarios to execute. Default is 500.
+	 */
 	public void setNumberOfRuns(int numberOfRuns) {
 		this.numberOfRuns = numberOfRuns;
 	}
 	
+	/**
+	 * Set numberOfConcurrentStarts before execution. 
+	 * @param numberOfConcurrentStarts the number of threads to be used for concurrent execution of scenarios. Default is 5.
+	 */
 	public void setNumberOfConcurrentStarts(int numberOfConcurrentStarts) {
 		this.numberOfConcurrentStarts = numberOfConcurrentStarts;
 	}
 
+	/**
+	 * Set scenarioWeightings before execution
+	 * @param scenarioWeightings a map keyed on the scenarios to execute, along with a relative weighting, represented by an integer. 
+	 * Default is null.
+	 */
 	public void setScenarioWeightings(Map<ScenarioBase, Integer> scenarioWeightings) {
 		this.scenarioWeightings = scenarioWeightings;
 	}
 
+	/**
+	 * Set scheduledExecutorService before execution
+	 * @param scheduledExecutorService an ExecutorService with a thread pool that can be used to submit scenarios for execution. A simple
+	 * implementation that can be used for this purpose can be got by new java.util.concurrent.ScheduledThreadPoolExecutor(<thread pool size>).
+	 * Default is null.
+	 */
 	public void setScheduledExecutorService(ScheduledExecutorService scheduledExecutorService) {
 		this.scheduledExecutorService = scheduledExecutorService;
 	}
 
+	/**
+	 * Set coolDownPeriod before execution
+	 * @param coolDownPeriod the duration, in milliseconds, that the runner should wait after initiating execution of the final scenario
+	 * for all scenarios to complete. If a scenario does not complete within the coolDownPeriod, it is marked as a failure. Default is 1000.
+	 */
 	public void setCoolDownPeriod(long coolDownPeriod) {
 		this.coolDownPeriod = coolDownPeriod;
 	}
 
+	/**
+	 * Set synchronizedScheduling before execution
+	 * @param synchronizedScheduling a boolean, which if set to true, means that when a thread executes one scenario, it does not immediately
+	 * start the next scenario until the other threads in the pool have finished execution of their respective scenarios. This guarantees that
+	 * the system under test will always have to initiate the same number of scenarios in parallel. Alternatively, if this value is set to false,
+	 * a thread will move on to the execution of the next scenario independent of other threads; this offers a more varied test of the system
+	 * and probably a different set of race conditions. Default is false.
+	 */
 	public void setSynchronizedScheduling(boolean synchronizedScheduling) {
 		this.synchronizedScheduling = synchronizedScheduling;
 	}
 
+	/**
+	 * Set synchronizeWaitMilliSeconds
+	 * @param synchronizeWaitMilliSeconds the maximum duration, in milliseconds, that a thread should wait for other threads to finish
+	 * execution of their respective scenarios to synchronize the start of the next scenario before assuming the other threads to be
+	 * infinite and progressing with its next scenario. Default is 1000.
+	 */
 	public void setSynchronizeWaitMilliSeconds(int synchronizeWaitMilliSeconds) {
 		this.synchronizeWaitMilliSeconds = synchronizeWaitMilliSeconds;
 	}
 
-	public long getTotalRunTimeMilliSeconds() {
-		return totalRunTimeMilliSeconds;
-	}
-	
 	private static class RunnableScenario implements Runnable {
 		ScenarioBase scenario;
 		String scenarioId;
